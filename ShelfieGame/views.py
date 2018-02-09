5e84c5d102b6908e0151d89490a6d7395d786e34
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2018-02-09 15:16:44 -0800

    updating game fans working

--- ShelfieGame/views.py
+++ ShelfieGame/views.py
@@ -9,6 +9,8 @@ from ShelfieUser.models import User
 from ShelfieChallenge.models import Challenge
 
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 from rest_framework import filters, generics, mixins
 from knox.auth import TokenAuthentication
 
@@ -41,9 +43,9 @@ class GameDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, gener
         return game
 
     def put(self, request, *args, **kwargs):
-        instance = get_object_or_404(Game, random_game_id=self.kwargs.pop('random_game_id'))
-        instance.fans.add(User.objects.get(random_user_id=request.data['random_user_id']))
-        return self.update(request, *args, **kwargs)
+        game = get_object_or_404(Game, random_game_id=self.kwargs.pop('random_game_id'))
+        game.fans.add(User.objects.get(random_user_id=request.data['random_user_id']))
+        return Response(HTTP_200_OK)
 
 class GameLeaderboardAPIView(generics.RetrieveAPIView):
     serializer_class = GameLeaderboardSerializer
