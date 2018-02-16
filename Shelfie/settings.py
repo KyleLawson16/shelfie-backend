9546b503adcb29ba152ed343f15b4f090c6dcde5
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2018-02-16 04:14:45 -0800

    see bugs

--- Shelfie/settings.py
+++ Shelfie/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.staticfiles',
     'raven.contrib.django.raven_compat',
 
 
