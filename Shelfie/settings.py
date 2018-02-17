98f1765613306502685df6eaadc7258654fb9af7
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2018-02-16 19:07:28 -0800

    using different bucket

--- Shelfie/settings.py
+++ Shelfie/settings.py
@@ -214,7 +214,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Amazon S3
 AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'shelfie-challenge-staging'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
